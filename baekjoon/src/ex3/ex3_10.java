a30754875528789d1e1910c03724a1df30a0c7f7
Geonsu Lee <legonsu2@gmail.com>
2022-07-28 23:39:02 +0900

    ex3_9 ~ 3_10

---
+++ baekjoon/src/ex3/ex3_10.java
@@ -0,0 +1,42 @@
+package ex3;
+
+import java.util.Scanner;
+
+public class ex3_10 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		sc.close();
+		
+		if(n<1 || n>100) {
+			while(true) {
+				System.out.println("1~100사이의 숫자를 입력해주세요");
+				n = sc.nextInt();
+				if(n>=1 && n<=100) {
+					break;
+				}
+			}
+		}
+		
+	
+		
+		
+		for(int i=1; i<=n; i++) {
+			
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int m=0; m<i; m++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+	}
+	
+}
