2375a3fe5d70c2e9563d1bf9b2498eca80340fd2
Geonsu Lee <legonsu2@gmail.com>
2022-08-13 23:25:06 +0900

    ex6_9, 6_10 어렵다 내일 다시 도전

---
+++ baekjoon/src/ex6/ex6_10.java
@@ -0,0 +1,27 @@
+package ex6;
+
+import java.util.Scanner;
+
+public class ex6_10 {
+
+	public static boolean check() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		boolean [] check = new boolean[26];
+		String st = sc.next();
+		
+
+		
+		
+		return false;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ baekjoon/src/ex6/ex6_9.java
@@ -0,0 +1,30 @@
+package ex6;
+
+import java.util.Scanner;
+
+public class ex6_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String st = sc.nextLine();
+		int count = 0;
+		
+		
+		for(int i=0; i<st.length(); i++) {
+			char ch = st.charAt(i);
+			
+			if(ch =='c') {
+				if(i<st.length()-1) {
+					
+				}
+			}
+			
+		}
+		
+		
+	}
+
+}
