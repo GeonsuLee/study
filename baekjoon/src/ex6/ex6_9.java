2e86742319c62fec65293de3295c6ad9085800b6
Geonsu Lee <legonsu2@gmail.com>
2022-08-14 23:28:35 +0900

    ex6_9

--- baekjoon/src/ex6/ex6_9.java
+++ baekjoon/src/ex6/ex6_9.java
@@ -13,18 +13,79 @@ public class ex6_9 {
 		int count = 0;
 		
 		
-		for(int i=0; i<st.length(); i++) {
+		for (int i=0; i<st.length(); i++) {
+			 
 			char ch = st.charAt(i);
-			
-			if(ch =='c') {
-				if(i<st.length()-1) {
-					
+ 
+			if(ch == 'c') {			
+				if(i < st.length() - 1) {
+					if(st.charAt(i + 1) == '=') {		
+						i++;	
+						
+					}else if(st.charAt(i + 1) == '-') {
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'd') {
+				if(i < st.length() - 1) {
+					if(st.charAt(i + 1) == 'z') {
+						if(i<st.length() - 2) {
+							if(st.charAt(i + 2) == '=') {
+								i += 2;
+							}
+						}
+						
+					}else if(st.charAt(i + 1) == '-') {
+						i++;
+						
+					}
 				}
 			}
 			
+			
+			
+		    
+			else if(ch == 'l') {
+				if(i<st.length() - 1) {
+					if(st.charAt(i + 1) == 'j') {
+						i++;
+						
+					}
+				}
+			}
+		    
+			else if(ch == 'n') {
+				if(i < st.length() - 1) {
+					if(st.charAt(i + 1) == 'j') {
+						i++;
+						
+					}
+				}
+			}
+ 
+			else if(ch == 's') {
+				if(i < st.length() - 1) {
+					if(st.charAt(i + 1) == '=') {
+						i++;
+						
+					}
+				}
+		    }
+ 
+			else if(ch == 'z') {
+				if(i < st.length() - 1) {
+					if(st.charAt(i + 1) == '=') {
+						i++;
+						
+					}
+				}
+			}
+		    
+			count++;
+ 
 		}
-		
-		
 	}
 
 }
