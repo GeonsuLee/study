07ebf2b9e2acaa9526096aac7a9a236aabfc5c91
Geonsu Lee <legonsu2@gmail.com>
2022-08-21 23:37:07 +0900

    ex7_3

--- baekjoon/src/ex7/ex7_3.java
+++ baekjoon/src/ex7/ex7_3.java
@@ -14,31 +14,38 @@ public class ex7_3 {
 		
 		int x = sc.nextInt();
 		
-		int sum = 1;
+		int sum = 0;
+		int cut = 1;
 		
-		// 1. 우선 어느칸에 있는지 확인을 해야된다.
+		// 우선 어느칸에 있는지 확인을 해야된다.
 		// 순차적으로 더해가며 증가.
 		
 		while(true) {
 			
-			sum+=sum;
-			if(x<=sum) {
 			
-			break;
+			if(x<=sum+cut) {
+			
+				// 2로 나눠서 짝수인지 홀수인지 확인
+				if(cut%2==1) {
+					int odddeno = cut-(x-sum-1);
+					int oddnume = x-sum;
+					System.out.println(odddeno+"/"+oddnume);
+					break;
+				
+				}else if(cut%2==0) {
+					int odddeno = x-sum;
+					int oddnume = cut-(x-sum-1);
+					System.out.println(odddeno+"/"+oddnume);
+					break;
+				}
+				
+
+			}else {
+				
+				sum+=cut;
+				cut++;
 			}
 		}
-		System.out.println(sum);
-		
-		if(x%2==0) {
-			
-			
-			
-		}else if(x%2==1) {
-			
-			
-			
-		}
-		
 		
 	}
 
