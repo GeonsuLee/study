22989319b2ed1391e5553937c54f1407e465e0ab
Geonsu Lee <legonsu2@gmail.com>
2022-08-22 23:28:27 +0900

    ex7_4

---
+++ baekjoon/src/ex7/ex7_4.java
@@ -0,0 +1,23 @@
+package ex7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex7_4 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		String[] abv = br.readLine().split(" ");
+		int a = Integer.parseInt(abv[0]);
+		int b = Integer.parseInt(abv[1]);
+		int v = Integer.parseInt(abv[2]);
+		
+		
+		
+	}
+
+}
